180a15f61c42975c6d0a8797069c578d53e28b7a
NBull92 <nickbull@nickbull-computing.biz>
2022-07-30 15:34:38 +0100

    get project setup

---
+++ EmployeeTracker/EmployeeTracker/EmployeeTracker.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    Company company = new Company("CompanyX");
+    std::cout << company.Name << "Employee Tracker!\n";
+}
+
+//  A class for the company. This will be instantiated in the main. But if the app was real, it could be generic company employee tracker
+//  Create a linked list of employees. 
+//  - Try to show case linked list knowledge to traverse, delete a node within the list, search the list, reverse the list (if you want to see employees in desc name order etc.
+
+
+// A class for employee
+//  - Name
+//  - Age
+//  - Salary
+//  - Bonus
---
+++ README.md
@@ -0,0 +1,3 @@
+Created to showcase my C++ and rebuild that skill set.
+- Started off as a console application
+- Stretch goal is to make a GUi application. Most likely with QT.
